1814689b3aa2aaac02287baae4038489e197a482
tslothorst <tslothorst@gmail.com>
2021-02-07 16:57:34 +0100

    Started section 4

---
+++ Section3/slicing-backwards/main.py
@@ -0,0 +1,8 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+tmp1 = letters[16:13:-1]  # output: qpo
+tmp2 = letters[4::-1]  # output: edcba
+tmp3 = letters[25:17:-1]  # output zyxwvuts
+
+print(tmp1)
+print(tmp2)
+print(tmp3)
